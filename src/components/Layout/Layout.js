a852f999903ce5a676d325a412ce31842b71d2e7
Andrew Circelli <andrewcircelli@gmail.com>
2021-02-19 21:44:12 -0500

    layout config

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+import { ThemeProvider } from "styled-components";
+import { themelight, themedark, themedarkblue } from "./theme";
+
+import GlobalStyle from "@src/styles/GlobalStyle";
+
+import ThemeToggleContext from "./ThemeToggleContext";
+
+import { setConfiguration } from "react-grid-system";
+setConfiguration({ breakpoints: [576, 769, 992, 1200] });
+
+const RootWrapper = styled(Wrapper)`
+  margin-top: 100px;
+  margin-bottom: 50px;
+  min-height: calc(100vh - 125px);
+
+  @media ${(props) => props.theme.media.tablet} {
+    margin-top: 50px;
+  }
+`;
+
+const Layout = ({ children }) => {
+  return (
+    <ThemeProvider theme={themelight}>
+      <>
+        <GlobalStyle />
+        <RootWrapper>{children}</RootWrapper>
+      </>
+    </ThemeProvider>
+  );
+};
+
+export default Layout;
